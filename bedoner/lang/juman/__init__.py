0a730c674227a371de133b1f3a28767adcc24c00
Yohei Tamura <tamuhey@gmail.com>
2019-08-29 23:43:50 +0900

    juman tokenizer

--- bedoner/lang/juman/__init__.py
+++ bedoner/lang/juman/__init__.py
@@ -1,7 +1,8 @@
 import re
-from typing import Optional
+from typing import Optional, List, Dict
 from collections import namedtuple
 
+from pyknp import Juman, Morpheme
 from .stop_words import STOP_WORDS
 from .tag_map import TAG_MAP
 from spacy.attrs import LANG
@@ -14,73 +15,31 @@ from spacy.util import DummyTokenizer
 ShortUnitWord = namedtuple("ShortUnitWord", ["surface", "lemma", "pos"])
 
 
-def try_mecab_import():
-    try:
-        import MeCab
-
-        return MeCab
-    except ImportError:
-        raise ImportError(
-            "Japanese support requires MeCab: "
-            "https://github.com/SamuraiT/mecab-python3"
-        )
-
-
-def resolve_pos(token):
-    """If necessary, add a field to the POS tag for UD mapping.
-    Under Universal Dependencies, sometimes the same Unidic POS tag can
-    be mapped differently depending on the literal token or its context
-    in the sentence. This function adds information to the POS tag to
-    resolve ambiguous mappings.
-    """
-    # TODO: This is a first take. The rules here are crude approximations.
-    # For many of these, full dependencies are needed to properly resolve
-    # PoS mappings.
-    if token.pos == "連体詞,*,*,*":
-        if re.match(r"[こそあど此其彼]の", token.surface):
-            return token.pos + ",DET"
-        if re.match(r"[こそあど此其彼]", token.surface):
-            return token.pos + ",PRON"
-        return token.pos + ",ADJ"
-    return token.pos
-
-
-def detailed_tokens(tokenizer, text):
+def detailed_tokens(tokenizer: Juman, text) -> List[Morpheme]:
     """Format Mecab output into a nice data structure, based on Janome."""
-    node = tokenizer.parseToNode(text)
-    node = node.next  # first node is beginning of sentence and empty, skip it
     words = []
-    while node.posid != 0:
-        surface = node.surface
-        base = surface  # a default value. Updated if available later.
-        parts = node.feature.split(",")
-        pos = ",".join(parts[0:4])
-        if len(parts) > 7:
-            # this information is only available for words in the tokenizer
-            # dictionary
-            base = parts[7]
-        words.append(ShortUnitWord(surface, base, pos))
-        node = node.next
+    ml = tokenizer.analysis(text)
+    for m in ml:
+        m: Morpheme = m
+        surface = m.midasi
+        pos = m.hinsi + "/" + m.bunrui
+        lemma = m.genkei or surface
+        words.append(ShortUnitWord(surface, lemma, pos))
     return words
 
 
 class Tokenizer(DummyTokenizer):
-    def __init__(self, cls, nlp: Optional[Language] = None, opt=""):
+    def __init__(self, cls, nlp: Optional[Language] = None, juman_kwargs={}):
         self.vocab = nlp.vocab if nlp is not None else cls.create_vocab(nlp)
-        self.tokenizer = try_mecab_import().Tagger(opt)
-        self.tokenizer.parseToNode("")  # see #2901
+        self.tokenizer = Juman(**juman_kwargs)
 
     def __call__(self, text):
         dtokens = detailed_tokens(self.tokenizer, text)
         words = [x.surface for x in dtokens]
         spaces = [False] * len(words)
         doc = Doc(self.vocab, words=words, spaces=spaces)
-        mecab_tags = []
         for token, dtoken in zip(doc, dtokens):
-            mecab_tags.append(dtoken.pos)
-            token.tag_ = resolve_pos(dtoken)
             token.lemma_ = dtoken.lemma
-        doc.user_data["mecab_tags"] = mecab_tags
         return doc
 
 
@@ -92,8 +51,8 @@ class Defaults(Language.Defaults):
     writing_system = {"direction": "ltr", "has_case": False, "has_letters": False}
 
     @classmethod
-    def create_tokenizer(cls, nlp=None, opt: str = ""):
-        return Tokenizer(cls, nlp, opt=opt)
+    def create_tokenizer(cls, nlp=None, juman_kwargs: Dict[str, str] = {}):
+        return Tokenizer(cls, nlp, juman_kwargs=juman_kwargs)
 
 
 class Japanese(Language):
