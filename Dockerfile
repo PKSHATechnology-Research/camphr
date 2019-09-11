bfed73c797181399ccce34e909bf2633c24e3622
tamuhey <tamuhey@gmail.com>
2019-09-11 20:12:06 +0900

    Ci: green! (#7)
    * fix ci# Please enter the commit message for your changes. Lines starting
    
    * fix# Please enter the commit message for your changes. Lines starting
    
    * fix# Please enter the commit message for your changes. Lines starting
    
    * fix
    
    * fix
    
    * fix

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM bedore/nlp-modules-base
 COPY . /app
 WORKDIR /app
-RUN pip install pipenv awscli && pipenv install --system --dev
+RUN pip install pipenv && pipenv install --dev && pipenv install -e .
---
+++ scripts/packaging.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import tempfile
+from spacy.cli import package
+
+additional_requirements = [
+    "bedoner @ git+https://github.com/PKSHATechnology/bedore-ner"
+]
+
+pkgs = Path(__file__).parent / "pkgs"
+
+
+def create_package(nlp):
+    meta = nlp.meta
+    req = meta.get("requirements") or []
+    req.append(additional_requirements)
+    nlp.meta["requirements"] = req
+
+    tmpd = tempfile.TemporaryDirectory()
+    nlp.to_disk(str(tmpd.name))
+    package(tmpd.name, pkgs, force=True)
+    model_name = meta["lang"] + "_" + meta["name"]
+    pkgd = pkgs / (model_name + "-" + meta["version"])
+    return pkgd, tmpd
