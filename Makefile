f980e5d6ff53bde8eb7227542294726c6675d1a5
Yohei Tamura <tamuhey@gmail.com>
2019-09-11 15:38:57 +0900

    up# Please enter the commit message for your changes. Lines starting

--- Makefile
+++
@@ -1,3 +0,0 @@
-lock:
-	pipenv lock -r > requirements.txt
-	pipenv lock -r --dev >> requirements.txt
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
-setup(packages=find_packages(where="./bedoner"))
+setup(packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]))
