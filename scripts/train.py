e821d4208aff3b8fbedb6f269818414421486317
Yohei Tamura <tamuhey@gmail.com>
2019-10-10 14:53:58 +0000

    fix train.py

--- scripts/train.py
+++ scripts/train.py
@@ -75,15 +75,20 @@ def main(cfg: Config):
         for j, batch in enumerate(minibatch(train_data, size=cfg.nbatch)):
             texts, golds = zip(*batch)
             docs = [nlp.make_doc(text) for text in texts]
-            nlp.update(docs, golds, optim)
+            try:
+                nlp.update(docs, golds, optim)
+            except:
+                with open("fail.json", "w") as f:
+                    json.dump(batch,f)
+                raise
             loss = sum(doc._.loss.detach().item() for doc in docs)
             epoch_loss += loss
             log.info(f"{j*cfg.nbatch}/{cfg.ndata} loss: {loss}")
             if j % 10 == 9:
                 scorer: Scorer = nlp.evaluate(val_data)
-                log.info("p: ", scorer.ents_p)
-                log.info("r: ", scorer.ents_r)
-                log.info("f: ", scorer.ents_f)
+                log.info(f"p: {scorer.ents_p}")
+                log.info(f"r: {scorer.ents_r}")
+                log.info(f"f: {scorer.ents_f}")
         log.info(f"epoch {i} loss: ", epoch_loss)
         scorer: Scorer = nlp.evaluate(val_data)
         nlp.meta = {"score": scorer.scores, "config": cfg.to_container()}
