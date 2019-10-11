55c07b44dbaf8f2b34a576a43f99e787762c79c1
Yohei Tamura <tamuhey@gmail.com>
2019-10-11 05:43:59 +0000

    fix upload github

--- scripts/upload_github_asset.py
+++ scripts/upload_github_asset.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import os
+import re
+import subprocess
 import fire
 import requests
 
@@ -14,8 +17,12 @@ def get_upload_url(tag, repo, token) -> str:
         ENDPOINT + f"/repos/{repo}/releases", headers=auth_header(token)
     ).json()
     for d in res:
-        if d["tag_name"] == tag:
-            return d["upload_url"]
+        try:
+            if d["tag_name"] == tag:
+                return d["upload_url"]
+        except:
+            print(res)
+            exit(1)
     raise ValueError(f"tag {tag} not found")
 
 
@@ -31,7 +38,32 @@ def make_header(file: str, token: str):
     return header
 
 
-def main(file, tag, repo, token):
+def get_repo() -> str:
+    remote_versbose = subprocess.check_output(["git", "remote", "-v"]).decode()
+    url = None
+    for line in remote_versbose.split("\n"):
+        items = line.split()
+        if not items:
+            continue
+        if items[0] == "origin":
+            url = items[1]
+    assert url
+    repo = re.findall(r"github.com[:/](.*?/.*)", url)[0]
+    repo = repo.rstrip(".git")
+    return repo
+
+
+def get_token() -> str:
+    token = os.getenv("GITHUB_TOKEN", "")
+    if not token:
+        token = input("Input github token: ")
+    return token
+
+
+def main(file, tag, token: str = ""):
+    token = get_token()
+    repo = get_repo()
+    print(f"repo: {repo}")
     url = get_upload_url(tag, repo, token)
     url = make_upload_url(url, file)
     header = make_header(file, token)
