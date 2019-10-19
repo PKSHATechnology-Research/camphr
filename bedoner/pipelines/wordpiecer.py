73d9a896a0d1f367565e7ed784e3f7b0a8d54b8d
Yohei Tamura <tamuhey@gmail.com>
2019-10-19 19:47:28 +0900

    Revert "cln: wordpiecer"
    This reverts commit e7f88cac310a8049578f0cc17c0cbc77f57fc5a9.

--- bedoner/pipelines/wordpiecer.py
+++ bedoner/pipelines/wordpiecer.py
@@ -1,12 +1,48 @@
 """Module wordpiecer defines wordpiecer for pytorch transformers."""
-from typing import Iterable, List
+from typing import Iterable, List, Type
 
+import transformers as trf
 from spacy.tokens import Doc
-from spacy_transformers.pipeline.wordpiecer import TransformersWordPiecer
+from spacy.vocab import Vocab
+from spacy_transformers.pipeline.wordpiecer import TransformersWordPiecer, get_tokenizer
+
+BERT_PRETRAINED_VOCAB_ARCHIVE_MAP = {
+    "bert-ja-juman": "s3://bedoner/trf_models/bert/bert-ja-juman-vocab.txt"  # bedore-ranndd aws account
+}
+PRETRAINED_INIT_CONFIGURATION = {
+    "bert-ja-juman": {
+        "do_lower_case": False,
+        "tokenize_chinese_chars": False,
+    }  # do_lower_case=False: 濁点落ちを防ぐ，tokenize_chinese_chars=False: スペース以外のspiltを防ぐ
+}
+PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES = {"bert-ja-juman": 512}
 
 
 class WordPiecer(TransformersWordPiecer):
     name = "trf_wordpiecer"
+
+    @classmethod
+    def from_pretrained(cls, vocab: Vocab, trf_name: str, **cfg):
+        """Instantiate wordpiecer from pretrained model.
+
+        Examples:
+            >>> nlp = WordPiecer.from_pretrained("bert-ja-juman")
+        """
+        trf_tokenizer_cls: Type[trf.PreTrainedTokenizer] = get_tokenizer(trf_name)
+
+        # tell `trf_tokenizer_cls` where to find the model
+        trf_tokenizer_cls.pretrained_vocab_files_map["vocab_file"].update(
+            BERT_PRETRAINED_VOCAB_ARCHIVE_MAP
+        )
+        # tell `trf_tokenizer_cls` how to configure the model
+        trf_tokenizer_cls.pretrained_init_configuration.update(
+            PRETRAINED_INIT_CONFIGURATION
+        )
+        trf_tokenizer_cls.max_model_input_sizes.update(
+            PRETRAINED_POSITIONAL_EMBEDDINGS_SIZES
+        )
+        model = trf_tokenizer_cls.from_pretrained(trf_name)
+        return cls(vocab, model=model, trf_name=trf_name, **cfg)
 
     def update(self, docs: Iterable[Doc], *args, **kwargs) -> List[Doc]:
         """Simply forward docs. This method is called when `spacy.Language.update`."""
