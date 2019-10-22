d3f4e779f4d3675ab3046ea2661ef45264036ec4
Yohei Tamura <tamuhey@gmail.com>
2019-10-23 00:55:57 +0900

    deleted:    scripts/foo.py

--- scripts/foo.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open("ene/foo.a") as f:
-    for line in f:
-        d = json.loads(line)
-        ents = d[1]["entities"]
-        if len(ents):
-            print(line, end="")
