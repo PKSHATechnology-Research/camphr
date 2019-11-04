3692df798f3a31287fdcbda101db5a222098af11
Yohei Tamura <tamuhey@gmail.com>
2019-11-04 18:59:34 +0900

    add pyknp

--- bedoner/pipelines/udify.py
+++ bedoner/pipelines/udify.py
@@ -15,8 +15,27 @@ class Udify(AllennlpPipe):
             uposes = output[UdifyOUTPUTS.upos]
             lemmas = output[UdifyOUTPUTS.lemmas]
             words = output[UdifyOUTPUTS.words]
-            assert words == [token.text for token in doc]
-            assert len(deps) == len(heads) == len(uposes) == len(words) == len(lemmas)
+            _doc_tokens = [token.text for token in doc]
+            if not words == [token.text for token in doc]:
+                raise ValueError(
+                    "Internal error has occured."
+                    f"Input text: {doc.text}\n"
+                    f"Input tokens: {_doc_tokens}\n"
+                    f"Model words: {words}"
+                )
+            if not (
+                len(deps) == len(heads) == len(uposes) == len(words) == len(lemmas)
+            ):
+                raise ValueError(
+                    "Internal error has occured.\n"
+                    f"text: {doc.text}\n"
+                    f"deps: {deps}\n"
+                    f"heads: {heads}\n"
+                    f"uposes: {uposes}\n"
+                    f"words: {words}\n"
+                    f"lemmas: {lemmas}\n"
+                )
+
             for token, dep, head, upos, lemma in zip(doc, deps, heads, uposes, lemmas):
                 token: Token = token
                 token.dep_ = dep
