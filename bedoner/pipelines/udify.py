20ea9b61a4f736bde02be4a1b6d3ac4fac70a33c
Yohei Tamura <tamuhey@gmail.com>
2019-11-04 12:57:55 +0900

    modified:   bedoner/pipelines/udify.py

--- bedoner/pipelines/udify.py
+++ bedoner/pipelines/udify.py
@@ -1,6 +1,7 @@
 from typing import Dict, Iterable
-from spacy.tokens import Doc
+from spacy.tokens import Doc, Token
 from allennlp.common.util import import_submodules
+from bedoner.vendor.udify.models.udify_model import OUTPUTS as UdifyOUTPUTS
 from .allennlp_base import AllennlpPipe
 
 import_submodules("bedoner.vendor.udify")
@@ -8,4 +9,17 @@ import_submodules("bedoner.vendor.udify")
 
 class Udify(AllennlpPipe):
     def set_annotation(self, docs: Iterable[Doc], outputs: Dict):
-        pass
+        for doc, output in zip(docs, outputs):
+            deps = output[UdifyOUTPUTS.predicted_dependencies]
+            heads = output[UdifyOUTPUTS.predicted_heads]
+            uposes = output[UdifyOUTPUTS.upos]
+            lemmas = output[UdifyOUTPUTS.lemmas]
+            words = output[UdifyOUTPUTS.words]
+            assert words == [token.text for token in doc]
+            assert len(deps) == len(heads) == len(uposes) == len(words) == len(lemmas)
+            for token, dep, head, lemma, upos in zip(doc, deps, heads, uposes, lemmas):
+                token: Token = token
+                token.dep_ = dep
+                token.lemma_ = lemma
+                token.pos_ = upos
+                token.head = doc[head]
