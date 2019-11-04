fe415f7e590c8e87ea02a7583a309a932226a550
Yohei Tamura <tamuhey@gmail.com>
2019-11-04 13:04:21 +0900

    new file:   tests/pipelines/test_udify.py

---
+++ tests/pipelines/test_udify.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import pytest
+from bedoner.pipelines.udify import Udify
+import bedoner.lang.mecab as mecab
+
+pytestmark = pytest.mark.skip("heavy test")
+
+
+@pytest.fixture
+def nlp():
+    _nlp = mecab.Japanese()
+    pipe = Udify.from_archive(Path(__file__).parent / "../../data/udify-model")
+    _nlp.add_pipe(pipe)
+    return _nlp
+
+
+def test_udify(nlp):
+    nlp("今日はいい天気だった")
