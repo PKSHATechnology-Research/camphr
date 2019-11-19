c2831b9c649ea2a1543ab47a9e02b551c913cab2
tamuhey <tamuhey@gmail.com>
2019-11-19 19:58:11 +0900

    Reup model for v0.4 (#125)
    * new file:   scripts/versionup_packages.py
    
    * new file:   models/models-v0.4.txt
    new file:   scripts/change_lang_cls.py
    
    * add metafiles

---
+++ scripts/versionup_packages.py
@@ -0,0 +1,12 @@
+import sys
+import subprocess
+from pathlib import Path
+
+pkgd = Path("pkgs")
+fmodels = sys.argv[1]
+models = list(open(fmodels).read().split())
+for model in models:
+    print(model)
+    path = pkgd / model / f"{model.split('-')[0]}" / model
+    if path.exists():
+        subprocess.run(["python", "scripts/versionup_package.py", str(path), "v0.4"])
