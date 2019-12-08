bea076036749224dce2b994ea4a60ad329540313
tamuhey <tamuhey@gmail.com>
2019-12-08 18:27:28 +0900

    refactor cuda test (#154)
    close #137

--- tests/pipelines/test_embedrank.py
+++ tests/pipelines/test_embedrank.py
@@ -1,17 +1,16 @@
 import pytest
 import spacy
-import torch
-
 from bedoner.models import trf_model
 from bedoner.pipelines.embedrank import EMBEDRANK_KEYPHRASES, EmbedRank
 
 
 @pytest.fixture(scope="module", params=["mecab"])
-def nlp(request, trf_dir):
+def nlp(request, trf_dir, device):
     lang = request.param
     _nlp = trf_model(lang, trf_dir)
     pipe = EmbedRank(vocab=_nlp.vocab)
     _nlp.add_pipe(pipe)
+    _nlp.to(device)
     return _nlp
 
 
@@ -20,13 +19,6 @@ TEXTS = ["今日はいい天気だった"]
 
 @pytest.mark.parametrize("text", TEXTS)
 def test_embedrank(nlp, text):
-    doc = nlp(text)
-    assert doc._.get(EMBEDRANK_KEYPHRASES) is not None
-
-
-@pytest.mark.parametrize("text", TEXTS)
-@pytest.mark.skipif(not torch.cuda.is_available(), reason="cuda test")
-def test_embedrank_with_cuda(nlp, text):
     doc = nlp(text)
     assert doc._.get(EMBEDRANK_KEYPHRASES) is not None
 
