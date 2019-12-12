ed52d25b41ca4c598ba068d3a3b35009e79f721d
Yohei Tamura <tamuhey@gmail.com>
2019-12-12 19:32:04 +0900

    add deprecation comment

--- bedoner/pipelines/knp_ner.py
+++ bedoner/pipelines/knp_ner.py
@@ -1,9 +1,14 @@
 import warnings
 
+NEW_MODULE = "bedoner.pipelines.knp"
+warnings.warn(
+    f"knp_ner has been deprecated. Please use {NEW_MODULE}.", DeprecationWarning
+)
+
 
 class KnpEntityExtractor:
     def __init__(self) -> None:
         warnings.warn(
-            "KnpEntityExtractor has been deprecated. Please use bedoner.pipelines.knp.KNP.",
+            f"KnpEntityExtractor has been deprecated. Please use {NEW_MODULE}.KNP.",
             DeprecationWarning,
         )
