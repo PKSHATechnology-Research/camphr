bee0f7d7f3adb6b59514bddb03eee2ddf94fc738
Yohei Tamura <tamuhey@gmail.com>
2020-03-02 15:03:34 +0900

    add test of long sentence for knp

--- tests/pipelines/test_knp.py
+++ tests/pipelines/test_knp.py
@@ -89,3 +89,7 @@ def test_knp_children_getter(
         children = [span._.get(KNP_USER_KEYS.tag.children) for span in spans]
         children = [[cc.text for cc in c] for c in children]
         assert children == children_texts
+
+
+def test_long_sentence(nlp: Language):
+    nlp("一" * 10000)
