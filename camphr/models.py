827b71c038c8ee92f31f3fa9b39ecffa525abb0b
Yohei Tamura <tamuhey@gmail.com>
2020-04-07 16:14:46 +0900

    fix models.py

--- camphr/models.py
+++ camphr/models.py
@@ -239,7 +239,7 @@ def _complement_trf_name(cfg: NLPConfig) -> NLPConfig:
             + cfg.pipeline.pretty()
         )
     for k, v in cfg.pipeline.items():
-        if k in TRF_PIPES:
+        if k in TRF_PIPES and KEY not in v:
             v[KEY] = VAL
     return cfg
 
