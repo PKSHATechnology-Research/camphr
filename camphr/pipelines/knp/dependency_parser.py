5c21958954d2b6da0e7ef8e09b8ff1bc8a193bcf
Yohei Tamura <tamuhey@gmail.com>
2020-04-13 11:36:55 +0900

    refactor knp dependency parser

--- camphr/pipelines/knp/dependency_parser.py
+++ camphr/pipelines/knp/dependency_parser.py
@@ -1,9 +1,22 @@
 """Convert KNP dependency parsing result to spacy format."""
-from typing import Iterable, Optional
+from typing import Any, Dict, Iterable, Optional
 
 import spacy
+from spacy.symbols import (
+    ADJ,
+    ADP,
+    ADV,
+    AUX,
+    CCONJ,
+    DET,
+    NOUN,
+    NUM,
+    PART,
+    PRON,
+    PUNCT,
+    VERB,
+)
 from spacy.tokens import Doc, Span, Token
-from spacy.symbols import ADJ, ADP, ADV, AUX, CCONJ, DET, NOUN, NUM, PRON, PART, PUNCT, VERB  # type: ignore
 
 from camphr.pipelines.knp import KNP_USER_KEYS
 
@@ -15,13 +28,13 @@ def knp_dependency_parser(doc: Doc) -> Doc:
         parent: Optional[Span] = tag._.get(KNP_USER_KEYS.tag.parent)
         if parent is not None:
             tag[0].head = parent[0]
-            tag[0].dep_ = knp_dependency_parser_head(tag[0])
+            tag[0].dep_ = _get_dep(tag[0])
         else:
             tag[0].head = tag[0]
             tag[0].dep_ = "ROOT"
         for c in tag[1:]:
             c.head = tag[0]
-            c.dep_ = knp_dependency_parser_func(c)
+            c.dep_ = _get_child_dep(c)
     doc.is_parsed = True
     return doc
 
@@ -30,51 +43,46 @@ def knp_dependency_parser_factory(*args, **kwargs):
     return knp_dependency_parser
 
 
-def knp_dependency_parser_head(tag: Token) -> str:
-    x = {ADV: "advmod", CCONJ: "advmod", DET: "det"}
-    if tag.pos in x:  # type: ignore
-        return x[tag.pos]  # type: ignore
-    elif tag.pos in [VERB, ADJ]:  # type: ignore
-        try:
-            f = tag._.knp_morph_tag._.knp_tag_element.features
-            if f["係"] == "連格":
-                return "acl"
-            return "advcl"
-        except (AttributeError, KeyError):
-            return "advcl"
-    try:
-        return knp_dependency_parser_noun(tag)
-    except (AttributeError, KeyError):
+def _get_dep(tag: Token) -> str:
+    ret = ({ADV: "advmod", CCONJ: "advmod", DET: "det"}).get(tag.pos)
+    if ret:
+        return ret
+    elif tag.pos in {VERB, ADJ}:
+        if tag._.knp_morph_tag._.knp_tag_element.features.get("係", "") == "連格":
+            return "acl"
+        return "advcl"
+    return _get_dep_noun(tag)
+
+
+def _get_dep_noun(tag: Token) -> str:
+    f: Dict[str, Any] = tag._.knp_morph_tag._.knp_tag_element.features
+    if "係" not in f and "解析格" not in f:
         return "dep"
-
-
-def knp_dependency_parser_noun(tag: Token) -> str:
-    f = tag._.knp_morph_tag._.knp_tag_element.features
     k = f["係"] if f["係"] != "未格" else f["解析格"] + "格"
     x = {"隣": "nmod", "文節内": "compound", "ガ格": "nsubj", "ヲ格": "obj"}
     if k in x:
         return x[k]
     elif k != "ノ格":
         return "obl"
-    if tag.head.pos in [VERB, ADJ]:  # type: ignore
+    if tag.head.pos in {VERB, ADJ}:
         return "nsubj"
-    elif tag.pos in [DET, PRON]:  # type: ignore
-        tag.pos = DET  # type: ignore
+    elif tag.pos in {DET, PRON}:
+        tag.pos = DET
         return "det"
     else:
         return "nmod"
 
 
-def knp_dependency_parser_func(tag: Token) -> str:
-    p, pp = tag.pos, tag.head.pos  # type: ignore
+def _get_child_dep(tag: Token) -> str:
+    p, pp = tag.pos, tag.head.pos
     if p == AUX:
-        return "aux" if pp in [VERB, ADJ] else "cop"
+        return "aux" if pp in {VERB, ADJ} else "cop"
     elif p == ADP:
-        return "mark" if pp in [VERB, ADJ] else "case"
-    elif p in [VERB, ADJ]:
+        return "mark" if pp in {VERB, ADJ} else "case"
+    elif p in {VERB, ADJ}:
         if pp == NOUN:
-            tag.head.pos = VERB  # type: ignore
-        tag.pos = AUX  # type: ignore
+            tag.head.pos = VERB
+        tag.pos = AUX
         return "aux"
     elif p == PART:
         return "mark"
