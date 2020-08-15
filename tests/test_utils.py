2ac9db9e6e0a8cf0b72776f3a11816081963ec63
Yohei Tamura <tamuhey@gmail.com>
2020-08-15 17:46:46 +0900

    fix get_doc_char_span and covering span (#78)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -42,16 +42,20 @@ def test_split_keepsep(text, sep, expected):
 
 
 @pytest.mark.parametrize(
-    "tokens,i,j,destructive,covering,expected",
+    "tokens,i,j,destructive,covering,expected,label",
     [
-        (["Foo", "bar", "baz"], 0, 2, True, False, "Fo"),
-        (["Foo", "bar", "baz"], 0, 5, True, False, "Foo b"),
-        (["Foo", "bar", "baz"], 0, 5, False, True, "Foo bar"),
-        (["Foo", "bar", "baz"], 0, 3, False, True, "Foo"),
-        (["Foo", "bar", "baz"], 1, 5, False, True, "Foo bar"),
+        (["Foo", "bar", "baz"], 0, 2, True, False, "Fo", None),
+        (["Foo", "bar", "baz"], 0, 5, True, False, "Foo b", None),
+        (["Foo", "bar", "baz"], 0, 5, False, True, "Foo bar", None),
+        (["Foo", "bar", "baz"], 0, 3, False, True, "Foo", None),
+        (["Foo", "bar", "baz"], 1, 5, False, True, "Foo bar", "LABEL"),
     ],
 )
-def test_get_doc_char_span(vocab, tokens, i, j, destructive, covering, expected):
+def test_get_doc_char_span(vocab, tokens, i, j, destructive, covering, expected, label):
     doc = Doc(vocab, tokens)
-    span = get_doc_char_span(doc, i, j, destructive=destructive, covering=covering)
+    span = get_doc_char_span(
+        doc, i, j, destructive=destructive, covering=covering, label=label or ""
+    )
     assert span.text == expected
+    if label is not None:
+        assert span.label_ == label
