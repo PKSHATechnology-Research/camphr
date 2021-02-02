536bc49481137c81169cacda7bc1f11ead3debbd
Yohei Tamura <tamuhey@gmail.com>
2021-02-02 18:43:37 +0900

    revert

--- camphr/pipelines/__init__.py
+++ camphr/pipelines/__init__.py
@@ -1,1 +1,10 @@
 # flake8: noqa
+from .pattern_search import PatternSearcher
+from .regex_ruler import MultipleRegexRuler, RegexRuler
+from .transformers import (
+    TrfForMultiLabelSequenceClassification,
+    TrfForNamedEntityRecognition,
+    TrfForSequenceClassification,
+    TrfModel,
+    TrfTokenizer,
+)
--- camphr/pipelines/transformers/__init__.py
+++ camphr/pipelines/transformers/__init__.py
@@ -1,1 +1,14 @@
 # flake8: noqa
+from .maskedlm import (
+    BertForMaskedLM,
+    BertForMaskedLMPreprocessor,
+    add_maskedlm_pipe,
+    remove_maskedlm_pipe,
+)
+from .model import TrfModel
+from .ner import TrfForNamedEntityRecognition
+from .seq_classification import (
+    TrfForMultiLabelSequenceClassification,
+    TrfForSequenceClassification,
+)
+from .tokenizer import TrfTokenizer
