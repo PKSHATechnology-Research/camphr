8096901ae8995d86956dc45d85d3ede7bcb07d30
Yohei Tamura <tamuhey@gmail.com>
2021-05-19 17:06:41 +0900

    ok: camphr_test

--- subpackages/camphr_pattern_search/tests/test_pattern_search.py
+++ subpackages/camphr_pattern_search/tests/test_pattern_search.py
@@ -8,14 +8,7 @@ from spacy.tokens import Doc
 from spacy.tokens.span import Span
 
 from camphr_pattern_search.pattern_search import PatternSearcher
-
-
-def check_mecab() -> bool:
-    try:
-        import MeCab  # noqa
-    except ImportError:
-        return False
-    return True
+from camphr_test.utils import check_mecab
 
 
 KEYWORDS = ["今日", "は", "明日", "lower", "mouse", "foobar", "走る", "頭痛", "BC", "AB ABC"]
