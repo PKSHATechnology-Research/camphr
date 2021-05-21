6d719be9c3d2d81c9691ce3d6cf029e8d4f6cdff
Yohei Tamura <tamuhey@gmail.com>
2021-05-21 16:01:11 +0900

    update deps

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,6 +6,9 @@ if [[ -z $1 ]]; then
     >&2 echo "ERROR: package name not specified"
     exit 1
 fi
+if [[ "$2" ]]; then
+  extras=$2
+fi
 
 function test_package() {
     poetry run mypy $1
@@ -17,7 +20,12 @@ set -x
 
 if [[ $1 == "all" ]]; then
     for subpackage in camphr_embedrank camphr_cli camphr_pattern_search; do
-        cd ./subpackages/${subpackage} && test_package ${subpackage} && cd ../..
+        cd ./subpackages/${subpackage} 
+        if [[ "$extras" ]]; then
+          poetry update $extras
+        fi
+        test_package ${subpackage} 
+        cd ../..
     done;
     test_package camphr
 else
