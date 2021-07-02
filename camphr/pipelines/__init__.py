4ccee9009ba843c805e68b70c17f4f8195dd7f4e
Yohei Tamura <tamuhey@gmail.com>
2021-07-02 14:46:33 +0900

    move pattern search to subpackage

--- camphr/pipelines/__init__.py
+++ camphr/pipelines/__init__.py
@@ -1,5 +1,4 @@
 # flake8: noqa
-from .pattern_search import PatternSearcher
 from .regex_ruler import MultipleRegexRuler, RegexRuler
 from .transformers import (
     TrfForMultiLabelSequenceClassification,
