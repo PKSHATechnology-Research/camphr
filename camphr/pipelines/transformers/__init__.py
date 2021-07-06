870bb2ba33eef36cd1e8bec705cbaf7a79facea0
Yohei Tamura <tamuhey@gmail.com>
2021-07-06 18:04:22 +0900

    remove pipelines

--- camphr/pipelines/transformers/__init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-from .model import TrfModel
-from .ner import TrfForNamedEntityRecognition
-from .seq_classification import (
-    TrfForMultiLabelSequenceClassification,
-    TrfForSequenceClassification,
-)
-from .tokenizer import TrfTokenizer
