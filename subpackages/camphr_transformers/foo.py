d62a5f6a4feb64753c415296e39851e17e20b04a
Yohei Tamura <tamuhey@gmail.com>
2021-07-12 13:05:04 +0900

    clean

--- subpackages/camphr_transformers/foo.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline, AutoTokenizer, AutoModelForTokenClassification
-
-pretrained_model_name_or_path = "./tests/fixtures/ner_bert"
-tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path)
-model = AutoModelForTokenClassification.from_pretrained(pretrained_model_name_or_path)
