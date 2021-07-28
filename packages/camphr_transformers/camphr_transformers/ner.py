fb4f72269d776ff64cee0d4dd38be5aedf46e309
Yohei Tamura <tamuhey@gmail.com>
2021-07-28 19:25:23 +0900

    wip: test

--- packages/camphr_transformers/camphr_transformers/ner.py
+++ packages/camphr_transformers/camphr_transformers/ner.py
@@ -1,14 +1,20 @@
-# Named entity recognition pipe component for transformers
-from typing import Any, Dict, List, cast, ClassVar
+"""Named entity recognition pipe component for transformers"""
+from typing import Any, ClassVar, Dict, List, Optional, Sequence, Tuple
+import textspan
 from typing_extensions import TypedDict
 from dataclasses import dataclass, asdict
 from camphr.serde import SerDe, SerDeDataclassMixin
 from pathlib import Path
-from camphr.doc import Doc, DocProto, Ent
+from camphr.doc import Doc, DocProto, Ent, Token
+import logging
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 from transformers.pipelines.token_classification import TokenClassificationPipeline
 from transformers.pipelines import pipeline as trf_pipeline
 from camphr.nlp import Nlp
+
+_SPECIAL_TOKENS_MASK = "special_tokens_mask"
+
+logger = logging.getLogger(__name__)
 
 
 class TrfEnt(TypedDict):
@@ -26,7 +32,7 @@ class Ner(Nlp, SerDe):
         tokenizer_kwargs: Dict[str, Any] = {},
         model_kwargs: Dict[str, Any] = {},
     ):
-        self.pipeline = self._create_pipeline(
+        self.tokenizer, self.model = self._create_pipeline(
             pretrained_model_name_or_path, tokenizer_kwargs, model_kwargs
         )
         self.tokenizer_kwargs = tokenizer_kwargs
@@ -37,36 +43,25 @@ class Ner(Nlp, SerDe):
         pretrained_model_name_or_path: str,
         tokenizer_kwargs: Dict[str, Any],
         model_kwargs: Dict[str, Any],
-    ) -> "TokenClassificationPipeline":
+    ) -> Tuple[AutoTokenizer, AutoModelForTokenClassification]:
         tokenizer = AutoTokenizer.from_pretrained(
             pretrained_model_name_or_path, **tokenizer_kwargs
         )
         model = AutoModelForTokenClassification.from_pretrained(
             pretrained_model_name_or_path, **model_kwargs
         )
-        pipeline: TokenClassificationPipeline = trf_pipeline(  # type: ignore
-            "ner", model=model, tokenizer=tokenizer  # type: ignore
-        )
-        if not isinstance(pipeline, TokenClassificationPipeline):
-            raise ValueError(
-                f"Internal Error: expected `TokenClassificationPipeline`, got {type(pipeline)}"
-            )
-        return pipeline
+        return tokenizer, model
 
-    def __call__(self, text: str) -> DocProto:
-        ents_raw = self.pipeline(text)
-        trf_ents = cast(List[TrfEnt], self.pipeline.group_entities(ents_raw))
-        doc = Doc(text)
-        doc.ents = []
-        for ent in trf_ents:
-            e = Ent(
-                ent["start"],
-                ent["end"],
-                doc,
-                label=ent["entity_group"],
-                score=ent["score"],
-            )
-            doc.ents.append(e)
+    def __call__(self, text: str) -> DocProto[Token, Ent]:
+        inputs = self.tokenizer(
+            text, return_tensors="pt", return_special_tokens_mask=True
+        )
+        tokens: List[str] = self.tokenizer.convert_ids_to_tokens(inputs["input_ids"][0])
+        mask: Sequence[int] = inputs[_SPECIAL_TOKENS_MASK][0]
+        del inputs[_SPECIAL_TOKENS_MASK]
+        output = self.model(**inputs).logits[0].argmax(1)
+        assert len(output.shape) == 1, output.shape
+        doc = _decode_bio(text, tokens, mask, output, self.model.config.id2label)
         return doc
 
     # ser/de
@@ -79,9 +74,58 @@ class Ner(Nlp, SerDe):
     def to_disk(self, path: Path):
         meta = self._SerdeMeta(self.tokenizer_kwargs, self.model_kwargs)
         meta.to_disk(path)
-        self.pipeline.save_pretrained(str(path))
+        self.tokenizer.save_pretrained(str(path))
 
     @classmethod
     def from_disk(cls, path: Path) -> "Ner":
         meta = cls._SerdeMeta.from_disk(path)
         return cls(str(path), **asdict(meta))
+
+
+def _decode_label(
+    mask: Sequence[int], output: Sequence[int], id2label: Dict[int, str]
+) -> List[str]:
+    ret: List[str] = []
+    assert len(mask) == len(output)
+    for mi, oi in zip(mask, output):
+        if int(mi):
+            continue
+        ret.append(id2label[int(oi)])
+    return ret
+
+
+def _decode_bio(
+    text: str,
+    tokens: List[str],
+    mask: Sequence[int],
+    output: Sequence[int],
+    id2label: Dict[int, str],
+) -> Doc:
+    doc = Doc(text)
+    labels = _decode_label(mask, output, id2label)
+    assert len(labels) == len(tokens), (labels, tokens)
+    new_tokens: List[Token] = []
+    ents: List[Ent] = []
+    cur_ent: Optional[Ent] = None
+    for span_lists, token, label in zip(
+        textspan.get_original_spans(tokens, text), tokens, labels
+    ):
+        if not span_lists:
+            logger.warn(f"Ignore token: {token}")
+            continue
+        l = span_lists[0][0]
+        r = span_lists[-1][1]
+        new_tokens.append(Token(start_char=l, end_char=r, doc=doc))
+        if label.startswith("I-") and cur_ent and cur_ent.label == label[2:]:
+            # expand previous entity
+            cur_ent.end_char = r
+        elif label.startswith("I-") or label.startswith("B-"):
+            # new entity
+            if cur_ent:
+                ents.append(cur_ent)
+            cur_ent = Ent(l, r, doc, label=label[2:])
+    if cur_ent:
+        ents.append(cur_ent)
+    doc.ents = ents
+    doc.tokens = new_tokens
+    return doc
