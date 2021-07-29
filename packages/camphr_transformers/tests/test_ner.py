971d0a6b80ef068bf4b834a83fbee2af2aca5083
Yohei Tamura <tamuhey@gmail.com>
2021-07-29 17:06:58 +0900

    update test

--- packages/camphr_transformers/tests/test_ner.py
+++ packages/camphr_transformers/tests/test_ner.py
@@ -1,25 +1,39 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Dict, List, Literal, Tuple, Set
+from typing import Dict, List, Literal, Tuple
+from typing_extensions import TypeAlias
 from camphr.serde import from_disk, to_disk
 import pytest
 from camphr_transformers.ner import Ner
 from .utils import FIXTURE_DIR
 
 
-SMALL_MODEL = str(FIXTURE_DIR / "ner_bert")
-TESTCASES_EN = [
-    (
-        "This is a test",
-        [("a", "ORG")],
-    ),
-]
+T_TESTCASE: TypeAlias = Tuple[str, List[Tuple[str, str]]]
 
 
 @dataclass
 class ModelInfo:
     size: Literal["small", "large"]
-    testcases: List[Tuple[str, List[Tuple[str, str]]]]
+    testcases: List[T_TESTCASE]
+
+
+SMALL_MODEL = str(FIXTURE_DIR / "ner_bert")
+TESTCASES_EN: List[T_TESTCASE] = [
+    (
+        "This is a test",
+        [("a", "ORG")],
+    ),
+    (
+        "暑熱厚",
+        [],
+    ),
+]
+TESTCASES_LARGE0: List[T_TESTCASE] = [
+    (
+        "Hugging Face Inc. is a company based in New York City.",
+        [("Hugging Face Inc", "ORG"), ("New York City", "LOC")],
+    )
+]
 
 
 @pytest.fixture(scope="session")
@@ -29,21 +43,21 @@ def nlp(request) -> Ner:
 
 SCENARIOS: Dict[str, ModelInfo] = {
     SMALL_MODEL: ModelInfo("small", TESTCASES_EN),
-    "dslim/bert-base-NER": ModelInfo("large", TESTCASES_EN),
+    "dslim/bert-base-NER": ModelInfo("large", TESTCASES_LARGE0),
 }
 
 
-SCENARIOS_RUN_NER: List[Tuple[str, str, List[Tuple[str, str]]]] = []
+PARAMS_RUN_NER: List[Tuple[str, str, List[Tuple[str, str]]]] = []
 for k, info in SCENARIOS.items():
     for case in info.testcases:
         param = (k, *case)
         if info.size == "large":
-            SCENARIOS_RUN_NER.append(pytest.param(*param, marks=pytest.mark.slow))
+            PARAMS_RUN_NER.append(pytest.param(*param, marks=pytest.mark.slow))  # type: ignore
         else:
-            SCENARIOS_RUN_NER.append(param)
+            PARAMS_RUN_NER.append(param)
 
 
-@pytest.mark.parametrize("nlp,text,expected", SCENARIOS_RUN_NER, indirect=["nlp"])
+@pytest.mark.parametrize("nlp,text,expected", PARAMS_RUN_NER, indirect=["nlp"])
 def test_ner(nlp: Ner, text: str, expected: List[Tuple[str, str]]):
     run_ner(nlp, text, expected)
 
@@ -55,10 +69,19 @@ def run_ner(nlp: Ner, text: str, expected: List[Tuple[str, str]]):
     assert ret == expected
 
 
-@pytest.mark.parametrize("nlp", [SMALL_MODEL], indirect=True)
-def test_serde(nlp: Ner, tmpdir: str):
+PARAMS_SERDE: List[Tuple[str, T_TESTCASE]] = []
+for k, info in SCENARIOS.items():
+    param = (k, info.testcases[0])
+    if info.size == "large":
+        PARAMS_SERDE.append(pytest.param(*param, marks=pytest.mark.slow))  # type: ignore
+    else:
+        PARAMS_SERDE.append(param)
+
+
+@pytest.mark.parametrize("nlp,case", PARAMS_SERDE, indirect=["nlp"])
+def test_serde(nlp: Ner, case: T_TESTCASE, tmpdir: str):
     path = Path(tmpdir)
     to_disk(nlp, path)
     nlp2 = from_disk(path)
     assert isinstance(nlp2, Ner)
-    run_ner(nlp2, *TESTCASES_EN[0])
+    run_ner(nlp2, *case)
