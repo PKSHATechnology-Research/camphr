7f5f45eef02395daa6cd71c4992ebce1ea437bde
Yohei Tamura <tamuhey@gmail.com>
2021-07-29 16:53:28 +0900

    update test scenario

--- packages/camphr_transformers/tests/test_ner.py
+++ packages/camphr_transformers/tests/test_ner.py
@@ -1,39 +1,61 @@
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Tuple, Set
+from typing import Dict, List, Literal, Tuple, Set
 from camphr.serde import from_disk, to_disk
 import pytest
 from camphr_transformers.ner import Ner
 from .utils import FIXTURE_DIR
 
 
-@pytest.fixture(scope="session")
-def nlp() -> Ner:
-    return Ner(str(FIXTURE_DIR / "ner_bert"))
-
-
+SMALL_MODEL = str(FIXTURE_DIR / "ner_bert")
 TESTCASES_EN = [
     (
         "This is a test",
-        {("This", "MISC"), ("is", "PER"), ("a", "ORG"), ("test", "PER")},
-    )
+        [("a", "ORG")],
+    ),
 ]
 
 
-@pytest.mark.parametrize("text, expected", TESTCASES_EN)
-def test_ner(nlp: Ner, text: str, expected: Set[Tuple[str, str]]):
+@dataclass
+class ModelInfo:
+    size: Literal["small", "large"]
+    testcases: List[Tuple[str, List[Tuple[str, str]]]]
+
+
+@pytest.fixture(scope="session")
+def nlp(request) -> Ner:
+    return Ner(request.param)
+
+
+SCENARIOS: Dict[str, ModelInfo] = {
+    SMALL_MODEL: ModelInfo("small", TESTCASES_EN),
+    "dslim/bert-base-NER": ModelInfo("large", TESTCASES_EN),
+}
+
+
+SCENARIOS_RUN_NER: List[Tuple[str, str, List[Tuple[str, str]]]] = []
+for k, info in SCENARIOS.items():
+    for case in info.testcases:
+        param = (k, *case)
+        if info.size == "large":
+            SCENARIOS_RUN_NER.append(pytest.param(*param, marks=pytest.mark.slow))
+        else:
+            SCENARIOS_RUN_NER.append(param)
+
+
+@pytest.mark.parametrize("nlp,text,expected", SCENARIOS_RUN_NER, indirect=["nlp"])
+def test_ner(nlp: Ner, text: str, expected: List[Tuple[str, str]]):
     run_ner(nlp, text, expected)
 
 
-def run_ner(nlp: Ner, text: str, expected: Set[Tuple[str, str]]):
+def run_ner(nlp: Ner, text: str, expected: List[Tuple[str, str]]):
     doc = nlp(text)
     assert doc.ents is not None
-    for e in doc.ents:
-        print(e.text, e.label)
-    for e in doc.ents:
-        entry = (e.text, e.label)
-        assert entry in expected
+    ret = [(e.text, e.label) for e in doc.ents]
+    assert ret == expected
 
 
+@pytest.mark.parametrize("nlp", [SMALL_MODEL], indirect=True)
 def test_serde(nlp: Ner, tmpdir: str):
     path = Path(tmpdir)
     to_disk(nlp, path)
