69fa6b3be40ac5b5c7bc226d5cb9f6d5f278a6db
Yohei Tamura <tamuhey@gmail.com>
2021-08-17 17:25:34 +0900

    add docs

--- packages/camphr/camphr/__init__.py
+++ packages/camphr/camphr/__init__.py
@@ -1,8 +1,12 @@
 """Package `camphr` provides several interfaces and utilities for other subpackages:
 
-- 
+- nlp.py: `Nlp` interface. Nlp may contain multiple `Pipe`s
+- pipe.py: `Pipe` interface. Simply `Doc -> Doc`
+- doc.py: `Doc` interface and implementations
+- serde.py: Serialization/Deserialization supports
 
-
+Typical users would implement `Nlp` and `SerDe`: a class takes a text and outputs a `DocProto`, and can be serialized/deserialized via `to_disk` and `from_disk`.
+All of the above entities are not concrete classes but interfaces that allow users to implement their own classes without inheriting anything.
 """
 from .VERSION import __version__
 
